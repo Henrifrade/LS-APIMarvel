718472814e8eccde7f7977c9ca2f1df7c2d3bb1b
Henrifrade <henriquegomesfrade@gmail.com>
2018-06-27 20:20:55 -0300

    pequenas mudanças no navbar, adicao do ant cod jquery de teste

--- js/main.js
+++ js/main.js
@@ -10,14 +10,20 @@ const baseEvents    = ``
 const baseCreators  = ``
 const baseCharacter = `characters?name=`
 
-// selecao dos ids do html
+// ============ Selecao dos ids do html ============= //
+
+// load-search
 let searchInputElm = document.querySelector('#search-input')
 let searchBtn = document.querySelector('#search-btn')
 let homeLoading = document.querySelector('#loading')
 let homeLoaded = document.querySelector('#loaded')
 let loadSearch = document.querySelector('#load-search')
-let footer = document.querySelector('#footer')
 
+// top-characters
+
+
+// footer
+let footer = document.querySelector('#footer')
 
 // ================== Funções =================== //
 
@@ -69,16 +75,14 @@ searchBtn.addEventListener('click', () => {
                 // TO DO
                 console.log('Fetch Erro')
             } else {
-                homeLoaded.innerHTML = searchResponse(searchChar)
-
                 console.log('Fetch OK')
+                homeLoaded.innerHTML = searchResponse(searchChar)
                 footer.innerHTML = searchChar.attributionHTML
                 homeLoading.style.display = 'none'
                 homeLoaded.style.display = 'grid'
             }
         })
 })
-
 
 // Event listerner de Enter e ESC
 searchInputElm.addEventListener('keyup', () => {
@@ -88,73 +92,3 @@ searchInputElm.addEventListener('keyup', () => {
         searchInputElm.value = ''
     }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* codigo feito usando jquery
-let marvel = {
-    render: function() {
-        let url = 'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=718d9655e79167925631daa64018feda&hash=dbe4e709801f86df2ba72f6ace4facad'
-        let footer = document.getElementById("footer")
-        let marvelContainer = document.getElementById("marvel-container")
-    
-    $.ajax({
-        type: "GET",
-        url: url,
-        beforeSend: function() {
-            console.log("Loading API")
-        },
-        complete: function() {
-            console.log("API Loaded!")
-        },
-        error: function() {
-            console.log("API não carregou!")
-        },
-        success: function(data) {
-            footer.innerHTML = data.attributionHTML
-
-            let string = ""
-            string += "<div class='row'>"
-
-            for(let i = 0; i < data.data.results.length; i++) {
-                let element = data.data.results[i]
-                
-                string += "<div class='card col-md-3 text-center'>"
-                string += "<img class='card-img-top' src='" + element.thumbnail.path + "/portrait_uncanny." + element.thumbnail.extension + "'></img>"
-                string += "<div class='card-body'>"
-                string += "<h5 class='card-title'>" + element.name + "</h5>"
-                string += "<h6 class='card-subtitle mb-2 tex-muted'>" + "ID: " + element.id + "</h6>"
-                string += "<a href='" + element.urls[0].url + "' class='btn btn-primary' target='_blank'>Link</a>"
-                string += "</div>"
-                string += "</div>"
-
-                if ((i + 1) % 4 == 0) {
-                    string += "</div>"
-                    string += "<div class='row'>"
-                }
-            }
-
-            marvelContainer.innerHTML = string
-        }
-    });
-
-    }
-}
-marvel.render()
-
-*/
